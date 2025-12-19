444d9ce8cbfae52858aacbc7d7d34b07bb4bc9c2
kiwikid1543-alt <kiwikid1543@gmail.com>
2025-12-19 09:46:56 +0900

    feat: 상단 이미지 UI구현

--- lib/main.dart
+++ lib/main.dart
@@ -42,62 +42,91 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Container(
-            width: 40,
-            height: 40,
-
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              // 컨테이너 사용시 컬러와 데코레이션 동시에 사용x, 데코레이션 안에 컬러를 넣거나?,
-              color: Colors.black,
-            ),
-            child: Icon(Icons.search, color: Colors.white),
-          ),
-          SizedBox(width: 16),
-        ],
-        // title 속성은 안드로이들에서 왼쪽 기본
-        // 아이폰에서는 가운데 기본
-        // 그걸 하나로 통일시켜주는게 centerTitle 속성
-        // actions에 위젯 배치되면 왼쪽으로 바뀜
-        // centerTitle: false, 주게되면 왼쪽으로 가지만
-        // title이 Column 을 배치하면 Column 크기가 앱바 영역전체를
-        // 차지하기 때문에 무용지물
-        // 이럴 땐 Column의 CrossAixsAlignment 속성으로 조정
-        // Column과 Row의 CrossAixsAlignment 기본값은 가운데
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: homeAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
           children: [
-            Text(
-              "Music",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 16,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
+            SizedBox(height: 20),
+            // 상단이미지
+            //
+            AspectRatio(
+              // 가로크기 / 세로크기
+              aspectRatio: 2.5 / 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20), //circular(원의 반지름)
+                child: Image.network(
+                  'https://picsum.photos/300/200',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(width: 3),
-                Container(
-                  width: 5,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                ),
-              ],
+              ),
             ),
+            // 제목행
+            // 큰앨범들어가는 로우
+            // 제목행
+            // 작은앨범들어가는 로우
           ],
         ),
       ),
       //
+    );
+  }
+
+  AppBar homeAppBar() {
+    return AppBar(
+      actions: [
+        Container(
+          width: 40,
+          height: 40,
+
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            // 컨테이너 사용시 컬러와 데코레이션 동시에 사용x, 데코레이션 안에 컬러를 넣거나?,
+            color: Colors.black,
+          ),
+          child: Icon(Icons.search, color: Colors.white),
+        ),
+        SizedBox(width: 16),
+      ],
+      // title 속성은 안드로이들에서 왼쪽 기본
+      // 아이폰에서는 가운데 기본
+      // 그걸 하나로 통일시켜주는게 centerTitle 속성
+      // actions에 위젯 배치되면 왼쪽으로 바뀜
+      // centerTitle: false, 주게되면 왼쪽으로 가지만
+      // title이 Column 을 배치하면 Column 크기가 앱바 영역전체를
+      // 차지하기 때문에 무용지물
+      // 이럴 땐 Column의 CrossAixsAlignment 속성으로 조정
+      // Column과 Row의 CrossAixsAlignment 기본값은 가운데
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Music",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 16,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              SizedBox(width: 3),
+              Container(
+                width: 5,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
