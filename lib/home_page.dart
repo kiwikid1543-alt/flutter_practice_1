8d579f97106d790b6208543fa5bea9d3e8120fe2
kiwikid1543-alt <kiwikid1543@gmail.com>
2025-12-26 09:41:57 +0900

    feat : 리스트부 빌더

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -59,37 +59,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     print("위젯 그리기 시작");
 
-    // Listview의 children 속성에 들어갈 리스트 만들기
-    List<Widget> listViewChildren = [];
-    // 반복문 이용해서 posList 내에 있는 요소 위젯으로 만들기
-    for (var index = 0; index < postList.length; index++) {
-      // children 속성에 들어갈 리스트에 추가해주기
-      Post p = postList[index];
-      Widget item = Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(),
-        ),
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("포스트 아이디 : ${p.id}"),
-            Text("사용자 아이디 : ${p.userId}"),
-            Text("제목 : ${p.title}"),
-            Text("내용 : ${p.body}"),
-          ],
-        ),
-      );
-      listViewChildren.add(item);
-    }
-
     return Scaffold(
       appBar: AppBar(title: Text('블로그 샘플')),
-      body: ListView(
+      // Listview.builer : itemCount 속성에 정의한 갯수만큼
+      // itemBuiler 속성에서 반환하는 위젯 보여주는 네임드 생성자!
+      body: ListView.builder(
         padding: EdgeInsets.all(20),
         //
-        children: listViewChildren,
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          //
+          print(index);
+          Post p = postList[index];
+          Widget item = Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(),
+            ),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("포스트 아이디 : ${p.id}"),
+                Text("사용자 아이디 : ${p.userId}"),
+                Text("제목 : ${p.title}"),
+                Text("내용 : ${p.body}"),
+              ],
+            ),
+          );
+          return item;
+        },
       ),
     );
   }
