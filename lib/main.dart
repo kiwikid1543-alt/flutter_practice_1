d5a644e2f827d60701b0f284589ba767350c25c8
kiwikid1543-alt <kiwikid1543@gmail.com>
2026-01-07 10:06:16 +0900

    feat: 파이어스토어 문서 쓰기

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_1/firebase_options.dart';
@@ -63,6 +64,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
+      // 여기서 matzip폴더(컬렉션)에 맛집파일(문서) 하나  추가할거임
+      // 1. 폴더 찾아갈 수 있는 행위 -> 컬렉션 참조 만들기
+      final colRef = FirebaseFirestore.instance.collection('matjip');
+
+      // 2. 위에서 만든 컬렉션 참조로 문서 만들기
+      Map<String, dynamic> data = {
+        '상호명': '이제모피자',
+        '주소': '부산광역시 부산진구',
+        '맛': '기대보다 별로',
+      };
+      colRef.add(data);
+
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
